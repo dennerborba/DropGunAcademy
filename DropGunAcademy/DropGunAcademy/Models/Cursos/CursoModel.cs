105b27b51176986372fa3ea9fe7c1fc721933106
lenilson.junior <lenilson.junior@BUE002D015>
2025-01-22 19:30:21 -0300

    Módulo de cursos

---
+++ DropGunAcademy/DropGunAcademy/Models/Cursos/CursoModel.cs
@@ -0,0 +1,10 @@
+﻿namespace DropGunAcademy.Models.Cursos
+{
+    public class CursoModel
+    {
+        public int ID { get; set; }
+        public string Tipo { get; set; }
+        public string Duracao { get; set; }
+        public Double Preco { get; set; }
+    }
+}
---
+++ DropGunAcademy/DropGunAcademy/Models/Cursos/CursosModel.cs
@@ -0,0 +1,10 @@
+﻿using DropGunAcademy.Models.Cursos;
+
+namespace DropGunAcademy.Models.Cursos
+{
+    public class CursosModel
+    {
+        public List<CursoModel> Cursos { get; set; }
+    }
+}
+
