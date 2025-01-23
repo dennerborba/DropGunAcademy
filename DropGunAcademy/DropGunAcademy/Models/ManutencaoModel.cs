4ddc445e7eaa630e07260b3df8725f496ee2e7a8
Alexjorge25 <alexandrojorgeantonio@gmail.com>
2025-01-22 21:13:37 -0300

    Criação da tela de manutenção das aeronaves
    Criação da tela de manutenção das aeronaves

---
+++ DropGunAcademy/DropGunAcademy/Models/ManutencaoModel.cs
@@ -0,0 +1,12 @@
+﻿namespace DropGunAcademy.Models
+{
+    public class ManutencaoModel
+    {
+        public int Id { get; set; }
+        public int IdAeronave { get; set; }
+        public DateOnly DataInicio { get; set; }
+        public DateOnly DataConclusao { get; set; }
+        public string Descricao { get; set; }
+        public string TecResp { get; set; }
+    }
+}
