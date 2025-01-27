c26ce402992adda2bc8405bdd9829b3d1b59717f
Alexjorge25 <alexandrojorgeantonio@gmail.com>
2025-01-27 20:26:55 -0300

    Restruturação
    Alteração para restruturação.

--- DropGunAcademy/DropGunAcademy/Controllers/HomeController.cs
+++ DropGunAcademy/DropGunAcademy/Controllers/HomeController.cs
@@ -18,7 +18,7 @@ namespace DropGunAcademy.Controllers
             return View();
         }
 
-        public IActionResult Privacy()
+        public IActionResult About()
         {
             return View();
         }
