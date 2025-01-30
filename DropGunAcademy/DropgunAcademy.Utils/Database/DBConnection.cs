b67f50793d5cf00a5102dd5d2668e6aa9a640500
dennerborba <dennerdeborba@gmail.com>
2025-01-30 20:45:15 -0300

    add utils conexão

---
+++ DropGunAcademy/DropgunAcademy.Utils/Database/DBConnection.cs
@@ -0,0 +1,31 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DropgunAcademy.Utils.Database
+{
+    public class DBConnection
+    {
+        public const string CONNECTION_STRING = "Server=localhost;Database=escola_aviacao;User ID=root;Password=@1@senac2021;";
+
+        public static bool TestarConexao()
+        {
+            try
+            {
+                using (MySqlConnection conn = new MySqlConnection(CONNECTION_STRING))
+                {
+                    conn.Open();
+                    return true;
+                }
+            }
+            catch
+            {
+                return false;
+            }
+        }
+    }
+}
+
