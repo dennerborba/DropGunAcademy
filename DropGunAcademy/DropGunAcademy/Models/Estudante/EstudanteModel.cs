c04bc61f5bbc9657d5ca0062156b2b87c9b98df9
Alexjorge25 <alexandrojorgeantonio@gmail.com>
2025-02-03 19:33:48 -0300

    Revert "atualizando"
    This reverts commit 9506206c2c59a4d5ec4e18b1feeec2ddbec7562c, reversing
    changes made to 4ce60bcfe013d0f1d1cb325b77ab16db97000cdd.

--- DropGunAcademy/DropGunAcademy/Models/Estudante/EstudanteModel.cs
+++ DropGunAcademy/DropGunAcademy/Models/Estudante/EstudanteModel.cs
@@ -2,22 +2,13 @@
 {
     public class EstudanteModel
     {
-        public int id_aluno { get; set; }
-        public int id_cidade_naturalidade { get; set; }
-        public int id_cidade_end { get; set; }
+        public int id { get; set; }
         public string nome { get; set; }
-        public string endereco { get; set; }
         public string cpf { get; set; }
-        public DateTime data_nascimento { get; set; }
-        public string end_cep { get; set; }
-        public string end_logradouro { get; set; }
-        public string end_complemento { get; set; }
-        public int end_numero { get; set; }
-        public string end_bairro { get; set; }
+        public DateTime dataNascimento { get; set; }
+        public string endereco { get; set; }
         public string telefone { get; set; }
         public string email { get; set; }
-        public DateTime data_cadastro { get; set; }
-        public DateTime data_alteracao { get; set; }
-        public int situacao_registro { get; set; }
+        public int status { get; set; }
     }
 }
