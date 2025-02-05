a0b3c427815cc69e52e402c174c76d46177141b8
dennerborba <dennerdeborba@gmail.com>
2025-02-05 20:52:45 -0300

    add Entidades + Aula /model

--- DropGunAcademy/DropGunAcademy/Models/Aulas/AulaModel.cs
+++ DropGunAcademy/DropGunAcademy/Models/Aulas/AulaModel.cs
@@ -4,7 +4,19 @@
     {
         public int Id { get; set; }
         public string NomeAula { get; set; }
-        public DateOnly DataAula { get; set; }
+        public DateTime DataAula { get; set; }
+        public int DuracaoAula { get; set; }
+        public int StatusAula { get; set; } 
+        public DateTime DataCadastro { get; set; }
+        public DateTime DataAlteracao { get; set; }
+        public byte situacao_registro { get; set; }
+
+
+        public AulaModel()
+        {
+            
+        }
+
 
     }
 }
